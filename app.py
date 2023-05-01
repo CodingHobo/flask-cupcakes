8740f174cb7f714b0d17c3d2c9c7f853deab6acd
Timothy Sukamtoh <timtoh168@gmail.com>
2023-05-01 12:08:18 -0700

    creating route and view functions

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
 """Flask app for Cupcakes"""
+
+import os
+
+from flask import Flask, render_template, redirect, flash, request
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import connect_db, db
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", "postgresql:///adopt")
+
+
+
+connect_db(app)
+
+@app.get("/api/cupcakes")
+def get_all_cupcakes():
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialized() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
